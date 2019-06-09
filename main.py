fcbffedd47942dcf9f828b5a26f5c8e5d83d78ba
HZRD <42003767+HZRDOUS@users.noreply.github.com>
2019-06-09 16:55:15 -0400

    More robust

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 from tkinter import *
 
 def clear():
-    pass
-
-
-def darkMode():
-    root.configure(background="black")
-    for wid in widgets:
-        if wid == mainframe:
-            wid.config(bg="black")
+    for var in vars:
+        if var == genderVar:
+            var.set("Male")
+        elif var == programVar:
+            var.set(programs[0])
+        elif var == monthVar or var == dayVar:
+            var.set(1)
+        elif var == yearVar:
+            var.set(2000)
         else:
-            wid.config(bg="black", fg="white")
+            var.set("")
 
-def lightMode():
-    root.configure(background=dbg)
-    for wid in widgets:
-        wid.config(bg=dbg, fg="black")
+
+#def darkMode():
+#    root.configure(background="black")
+#    for wid in widgets:
+#        if wid == mainframe:
+#            wid.config(bg="black")
+#        else:
+#            wid.config(bg="black", fg="white")
+
+#def lightMode():
+#    root.configure(background=dbg)
+#    for wid in widgets:
+#        wid.config(bg=dbg, fg="black")
 
 root = Tk()
+root.title("Camp Kidney Registration 2019")
+root.iconbitmap()
 dbg = root.cget("background")
 mainframe = Frame(root)
+infoFrame = Frame(mainframe)
+regframe = Frame(mainframe)
 
 # Define menu widgets
 menu = Menu(root)
@@ -29,72 +43,178 @@ root.config(menu=menu)
 filemenu = Menu(menu)
 menu.add_cascade(label="Edit", menu=filemenu)
 filemenu.add_command(label="Clear", command = clear)
-filemenu.add_command(label="Dark Mode", command = darkMode)
-filemenu.add_command(label="Light Mode", command = lightMode)
+#filemenu.add_command(label="Dark Mode", command = darkMode)
+#filemenu.add_command(label="Light Mode", command = lightMode)
 
-campLabel = Label(mainframe, text="Camp Kidney", font = ("Courier New", 20))
-campLabel2 = Label(mainframe, text="Registration Program", font = ("Courier New", 18))
-infoLabel = Label(mainframe, text="Kid Info", font = ("Courier New", 20))
-nameLabel = Label(mainframe, text="Full name:", font = ("Courier New", 10))
-dateOfBirthLabel = Label(mainframe, text="Date of Birth:", font = ("Courier New", 10))
-monthLabel = Label(mainframe, text="Month:", font = ("Courier New", 10))
-dayLabel = Label(mainframe, text="Day:", font = ("Courier New", 10))
-yearLabel = Label(mainframe, text="Year:", font = ("Courier New", 10))
-genderLabel = Label(mainframe, text="Gender:", font = ("Courier New", 10))
-programLabel = Label(mainframe, text="Program", font = ("Courier New", 10))
+campLabel = Label(infoFrame, text="Camp Kidney", font = ("Courier New", 20))
+campLabel2 = Label(infoFrame, text="Registration Program", font = ("Courier New", 18))
+infoLabel = Label(regframe, text="Kid Info", font = ("Courier New", 20))
+nameLabel = Label(infoFrame, text="Full name:", font = ("Courier New", 10))
+dateOfBirthLabel = Label(infoFrame, text="Date of Birth:", font = ("Courier New", 10))
+genderLabel = Label(infoFrame, text="Gender:", font = ("Courier New", 10))
 
 months = list(range(1, 12))
 days = list(range(1, 31))
 daysOdd = list(range(1, 30))
 daysFeb = list(range(1, 28))
 years = list(range(2000, 2019))
+#Main Labelframe Declaration
+kidInfo = LabelFrame(infoFrame, text = "Child Info", font = ("Courier New", 10))
+homeInfo = LabelFrame(infoFrame, text = "Home Info", font = ("Courier New", 10))
 
-nameVar = StringVar()
-nameEntry = Entry(mainframe)
+#Kidinfo
+#Name
+nameFrame = LabelFrame(kidInfo, text="Full Name:", font = ("Courier New", 10))
+firstNameVar = StringVar()
+firstNameChildLabel = Label(nameFrame, text="First Name:", font = ("Courier New", 8))
+firstNameEntry = Entry(nameFrame)
+lastNameVar = StringVar()
+lastNameChildLabel = Label(nameFrame, text="Last Name:", font = ("Courier New", 8))
+lastNameEntry = Entry(nameFrame)
 
+
+#Date of Birth
+dateFrame = LabelFrame(kidInfo, text = "Date of Birth:", font = ("Courier New", 10))
 monthVar = IntVar()
-monthEntry = Spinbox(mainframe, textvariable = monthVar, values=months)
+monthEntry = Spinbox(dateFrame, textvariable = monthVar, values=months, width = 6)
 dayVar = IntVar()
-dayEntry = Spinbox(mainframe, textvariable = dayVar, values = days)
+dayEntry = Spinbox(dateFrame, textvariable = dayVar, values = days, width = 6)
 yearVar = IntVar()
-yearEntry = Spinbox(mainframe, textvariable = yearVar, values = years)
-
-genderFrame = LabelFrame(mainframe, text="Gender:")
+yearEntry = Spinbox(dateFrame, textvariable = yearVar, values = years, width = 6)
+dayLabel = Label(dateFrame, text="Day", font = ("Courier New", 10))
+monthLabel = Label(dateFrame, text="Month", font = ("Courier New", 10))
+yearLabel = Label(dateFrame, text="Year", font = ("Courier New", 10))
+#Gender
+genderFrame = LabelFrame(kidInfo, text="Gender:", font = ("Courier New", 10))
 genderVar = StringVar()
 genderVar.set("Male")
 maleRadio = Radiobutton(genderFrame, text="Male", variable = genderVar, value="Male")
 femaleRadio = Radiobutton(genderFrame, text="Female", variable = genderVar, value="Female")
-
+otherRadio = Radiobutton(genderFrame, text="Other", variable = genderVar, value="Other")
+#Program
+programFrame = LabelFrame(kidInfo, text="Program:", font = ("Courier New", 10))
 programVar = StringVar()
-programVar.set("-----")
-programs = ["------", "Session 1", "Session 2", "Session 3", "Fishing with Garrison Stokes", 
-"Pokemon Camp", "The Birds and Bees Talk", "Programming with Mr. Dani Shaft", "Rap Talk with NAV",
-"Just Video Games", "Anime", "Advanced Functions MHF4U", ""]
+programVar.set("--- Select Program ---")
+programs = ["--- Select Program ---", "Session 1", "Session 2", "Session 3", "Fishing with Garrison Stokes", 
+            "Pokemon Camp", "The Birds and Bees Talk", "Programming with Mr. Dani Shaft", 
+            "Rap Talk with Hopsin", "Just Video Games", "Anime", "Advanced Functions MHF4U", 
+            ""]
+programMenu = OptionMenu(programFrame, programVar, *programs)
+programMenu.configure(font = ("Courier New", 10))
 
-scrollbar = Scrollbar(mainframe, command=canvas.yview)
-canvas.configure(yscrollcommand = scrollbar.set)
+#HomeInfo
+primaryInfoFrame = LabelFrame(homeInfo, text="Primary Home Info", font = ("Courier New", 10))
+addressVar = StringVar()
+addressLabel = Label(primaryInfoFrame, text="Address:", font = ("Courier New", 10))
+addressEntry = Entry(primaryInfoFrame, width = 60, textvariable = addressVar)
+cityVar = StringVar()
+cityLabel = Label(primaryInfoFrame, text="City/Town:", font = ("Courier New", 10))
+cityEntry = Entry(primaryInfoFrame, textvariable = cityVar)
+provinceVar = StringVar()
+provinceLabel = Label(primaryInfoFrame, text="Province/State:", font = ("Courier New", 10))
+provinceEntry = Entry(primaryInfoFrame, textvariable = provinceVar)
+countryVar = StringVar()
+countryLabel = Label(primaryInfoFrame, text="Country:", font = ("Courier New", 10))
+countryEntry = Entry(primaryInfoFrame, textvariable = countryVar)
+postalVar = StringVar()
+postalLabel = Label(primaryInfoFrame, text="Postal/Zip Code:", font = ("Courier New", 10))
+postalEntry = Entry(primaryInfoFrame, textvariable = postalVar)
+phoneVar = StringVar()
+phoneLabel = Label(primaryInfoFrame, text="Phone Number:", font = ("Courier New", 10))
+phoneEntry = Entry(primaryInfoFrame, width = 60, textvariable = phoneVar)
 
-widgets = [mainframe, infoLabel, menu, filemenu, campLabel, nameLabel, nameEntry, dateOfBirthLabel]
+#Parent Info
+parentInfoFrame = LabelFrame(homeInfo, text="Parent Info", font = ("Courier New", 10))
+parent1Frame = LabelFrame(parentInfoFrame, text="Parent 1:", font = ("Courier New", 10))
+parent2Frame = LabelFrame(parentInfoFrame, text="Parent 2:", font = ("Courier New", 10))
+genderOption1 = LabelFrame(parent1Frame, text="Parent/Guardian #1:", font = ("Courier New", 10))
+parentVar1 = StringVar()
+mrCheck1 = Radiobutton(parent1Frame, text="Mr.", variable=parentVar1, value="Mr.")
+mrsCheck1 = Radiobutton(parent1Frame, text="Mrs.", variable=parentVar1, value="Mrs.")
+msCheck1 = Radiobutton(parent1Frame, text="Ms.", variable=parentVar1, value="Ms.")
+missCheck1 = Radiobutton(parent1Frame, text="Miss.", variable=parentVar1, value="Miss.")
+drCheck1 = Radiobutton(parent1Frame, text="Dr.", variable=parentVar1, value="Dr.")
 
+genderOption2 = LabelFrame(parent2Frame, text="Parent/Guardian #1:", font = ("Courier New", 10))
+parentVar2 = StringVar()
+mrCheck2 = Radiobutton(parent2Frame, text="Mr.", variable=parentVar2, value="Mr.")
+mrsCheck2 = Radiobutton(parent2Frame, text="Mrs.", variable=parentVar2, value="Mrs.")
+msCheck2 = Radiobutton(parent2Frame, text="Ms.", variable=parentVar2, value="Ms.")
+missCheck2 = Radiobutton(parent2Frame, text="Miss.", variable=parentVar2, value="Miss.")
+drCheck2 = Radiobutton(parent2Frame, text="Dr.", variable=parentVar2, value="Dr.")
+
+
+vars = [firstNameVar, lastNameVar, monthVar, dayVar, yearVar, genderVar, programVar, addressVar,
+        cityVar, provinceVar, countryVar, postalVar, phoneVar]
+
+widgets = [mainframe, infoLabel, menu, filemenu, campLabel, nameLabel, firstNameEntry, 
+           lastNameEntry, dateOfBirthLabel]
 #Grid
-root.minsize(width=900, height=700)
-root.maxsize(width=1000, height=900)
-mainframe.grid(padx = 10, pady = 10)
+root.minsize(width=1100, height=700)
+root.maxsize(width=1100, height=900)
+#Frame Gridding
+mainframe.grid(row=1, column = 1, padx = 10, pady = 10)
+infoFrame.grid(row = 1, column = 1)
+regframe.grid(row=1, column=2)
+#Main Labelframe Gridding
+kidInfo.grid(row=3, column = 1)
+homeInfo.grid(row=4, column=1)
+#Heading Gridding
 campLabel.grid(row=1, column=1, padx = 0, pady = 0)
 campLabel2.grid(row=2, column=1, padx = 10)
-infoLabel.grid(row=1, column=2, padx = 30, pady = 10)
-nameLabel.grid(row=3, column=1, sticky = "W")
-nameEntry.grid(row=4, column=1)
-dateOfBirthLabel.grid(row=5, column=1, sticky = "W")
-dayLabel.grid(row=6, column=1)
-dayEntry.grid(row=7, column = 1)
-monthLabel.grid(row=8, column=1)
-monthEntry.grid(row=9, column=1)
-yearLabel.grid(row=10, column=1)
-yearEntry.grid(row=11, column = 1)
-genderLabel.grid(row=12, column=1, sticky="W")
-genderFrame.grid(row=13, column=1)
-maleRadio.grid(sticky="W")
-femaleRadio.grid(sticky="W")
 
+#infoFrame gridding
+infoLabel.grid(row=1, column=1)
+#Kidinfo Gridding
+nameFrame.grid(row=1, column=1, rowspan = 4, padx = 10, pady = 10)
+firstNameChildLabel.grid(row=1, column=1)
+firstNameEntry.grid(row=2, column=1)
+lastNameChildLabel.grid(row=3, column=1)
+lastNameEntry.grid(row=4, column=1)
+dateFrame.grid(row=1, column=2)
+dayLabel.grid(row=1, column=1)
+dayEntry.grid(row=2, column = 1)
+monthLabel.grid(row=1, column=2)
+monthEntry.grid(row=2, column=2)
+yearLabel.grid(row=1, column=3)
+yearEntry.grid(row=2, column = 3)
+genderFrame.grid(row=1, column=3)
+#Gender gridding
+maleRadio.grid(row = 1, column = 1, sticky="W")
+femaleRadio.grid(row = 1, column = 2, sticky="W")
+otherRadio.grid(row = 1, column = 3, sticky="W")
+programFrame.grid(row=4, column=2, columnspan = 3, padx = 10, pady = 10)
+programMenu.grid(row=1, column=1)
+
+#homeInfo gridding
+#Home Info
+primaryInfoFrame.grid(row=1, column=1)
+addressLabel.grid(row=1, column=1)
+addressEntry.grid(row=1, column=2, columnspan = 3)
+cityLabel.grid(row = 2, column=1)
+cityEntry.grid(row = 2, column=2)
+provinceLabel.grid(row = 2, column = 3)
+provinceEntry.grid(row = 2, column = 4)
+countryLabel.grid(row = 3, column = 1)
+countryEntry.grid(row = 3, column = 2)
+postalLabel.grid(row = 3, column = 3)
+postalEntry.grid(row = 3, column = 4)
+phoneLabel.grid(row = 4, column = 1)
+phoneEntry.grid(row = 4, column = 2, columnspan = 3)
+#Parent Info
+parentInfoFrame.grid(row = 2, column = 1)
+parent1Frame.grid(row = 1, column = 1)
+parent2Frame.grid(row = 1, column = 2)
+genderOption1.grid(row = 1, column = 1)
+mrCheck1.grid(row = 1, column = 1)
+mrsCheck1.grid(row = 1, column = 2)
+msCheck1.grid(row = 1, column = 3)
+missCheck1.grid(row=1, column = 4)
+drCheck1.grid(row=1, column = 5)
+genderOption2.grid(row = 1, column = 1)
+mrCheck2.grid(row = 1, column = 1)
+mrsCheck2.grid(row = 1, column = 2)
+msCheck2.grid(row = 1, column = 3)
+missCheck2.grid(row=1, column = 4)
+drCheck2.grid(row=1, column = 5)
 root.mainloop()
